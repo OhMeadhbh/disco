7f8b787c913bef9903196a837fb55134fff5c4d9
Meadhbh Hamrick <OhMeadhbh@gmail.com>
2015-12-08 04:10:26 +0000

    fixing origin issue

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function _display_intro() {
   var a = setInterval( function () {
     if( _$App.registry[ '_$HTMLReader' ] ) {
       localStorage.setItem( '_$Discovery_intro', 'seen it' );
-      _$App.launch( '_$HTMLReader', { url: '/contents/intro.html', height: _$.byId( 'root' ).offsetHeight - 41, width: 600, x: 12, y: 12, title: '¡hola!' } );
+      _$App.launch( '_$HTMLReader', { url: 'contents/intro.html', height: _$.byId( 'root' ).offsetHeight - 41, width: 600, x: 12, y: 12, title: '¡hola!' } );
     }
     clearInterval( a );
   }, 1000 );
